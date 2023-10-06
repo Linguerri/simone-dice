50005fa27c42305c1a67d0272ec43fc267954844
Linguerri <linguerrigiulia28@gmail.com>
2023-10-06 16:56:47 +0200

    try and modify

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -1,3 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>azione_simoDice/behavior.xar:/Say</name>
+        <message>
+            <source>Ciao</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="azione_simoDice/behavior.xar" line="0"/>
+            <source>iniziamo il gioco</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
--- translations/translation_it_IT.ts
+++ translations/translation_it_IT.ts
@@ -1,3 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it_IT"/>
+<TS version="2.1" language="it_IT">
+    <context>
+        <name>azione_simoDice/behavior.xar:/Say</name>
+        <message>
+            <source>Ciao</source>
+            <comment>Text</comment>
+            <translation type="vanished">Ciao</translation>
+        </message>
+        <message>
+            <source>ok iniziamo il gioco</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ok iniziamo il gioco</translation>
+        </message>
+        <message>
+            <location filename="azione_simoDice/behavior.xar" line="0"/>
+            <source>iniziamo il gioco</source>
+            <comment>Text</comment>
+            <translation type="unfinished">iniziamo il gioco</translation>
+        </message>
+    </context>
+</TS>
